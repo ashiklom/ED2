ba4958edc76d62108f36636bc9e712c8a2be1425
Naomi Marcil Levine <n.levine@usc.edu>
2012-09-04 15:35:00 -0400

    Changes made by M Longo, R Knox, A Swann, and N Levine
    BRAMS
    - Several bug fixes in static control for Grell cumulus' parameterization
    - Added option QI for dynamic control, which is just an average between 
         Arakawa-Schubert and Kain-Fritsch.
    
    ED
    Drought phenology:
    - Changes in the order in which plants burn carbon in drought phenology.
         If phenology_status = -1, then they first lose leaves, not storage. 
         If phenology_status=0 or 1, then they burn storage before they start
         losing living tissues.
    - Maximum light photosynthesis is done using the top cohort that has 
         leaves, not the top cohort that is resolvable. They are synonyms 
         when IBRANCH_THERMO is 0, but not when it is 1 or 2.
    
    Mortality:
    - Use the log of population density when we apply mortality rates 
         (dN/dt = -m*N, so the linear form is d lnN/dt = -m). 
    - Added mortality due to disturbance as a 5th mortality rate in cpatch%mort.
         This is never applied to the cohorts, it is for diagnostics only.
    - Reverted the density-dependent mortality rate coefficients back to 
         Moorcroft et al. (2001) numbers. Sapwood cannot be depleted when 
         plants go to negative carbon balance, which is in line with 
         Moorcroft et al. (2001).
    - DDMORT_CONST: new variable in ED2IN. If it is 1, then density-dependent
         mortality rate is the ED default (CBR = CB/CB_lightMax). If it is 
         zero, CBR = CB/CB_smoistMax, and any number between 0 and 1 will do
         a weighted average. 
    - IDDMORT_SCHEME. 0 is the default ED, 1 means that carbon balance is 
         offset by storage (to give more chances for drought deciduous)
    
    New diagnostic variables:
    - cpatch%recruit_status, to make comparison with census data easier. 
         0 means that the cohort is still small, 1 means new recruit 
         (first time with DBH > 10cm), and 2 means established (2nd+ time with
         DBH > 10cm). 
    - Saved the change in log of DBH/AGB/BA, which is more comparable to 
         Condit et al. (2006) growth rates.
    
    Big leaf:
    - Added biomass init. option for big leaf ED so we can use biometric data. 
    - Replaced dbh_crit by dbh_bigleaf when we run bigleaf, which makes AGB and
         LAI closer to obs. 
    - Photosynthetic parameters re-tuned for big leaf agains Manaus data
    *** Big-leaf ED now looks good (bug free) for the tropics and gives 
    reasonable results when run with either iallom=1 or iallom=2 without fire***
    
    Soil:
    - Small changes in ISOILBC. 0 (bedrock) and 1 (free vertical drainage) are 
         the exact same options. 2 became "lateral" drainage (which is just
         free drainage reduced by a slope coefficient, defined by SLDRAIN). 
         3 is the aquifer. I removed "super drainage" and the field capacity 
         options since they were just old tests. 
    - "New" decomposition scheme, which is actually based on the original ED-1.0
         formulation. I used the shape of ED-1.0 functions but wrote a 
         exponential based one to sqrt of negative numbers. The new scheme 
         works with DECOMP_SCHEME=2, other options stayed the same.
    - For DECOMP_SCHEME=2 only, I changed the turnover rate of slow carbon, 
         which was non-sense (100.2 /yr). I switched by 0.2/yr, which is more
         typical of the slow pools from CENTURY model. The decay rates were 
         renamed from K1, K2, K3 to something more intuitive (decay_rate_fsc,
         decay_rate_stsc, decay_rate_ssc)
    - New diagnostic variable: heterotrophic respiration from CWD. The 
         instantaneous value already existed, now it saves all the dmean, 
         qmean, avg stuff. 
    
    Other:
    - Added a new fire model that is entirely independent of soil moisture, 
         the threshold is given by accumulated water deficit (INCLUDE_FIRE=3). 
    - Small correction in selective logging, there was a bogus 0.5 factor due to
         a mistake in the derivation. Also changed the weight for plants so it 
         related to basal area (this should only affect simulations using 
         SimAmazonia2, which has information about logged area, but no target biomass).

--- src/io/ed_opspec.F90
+++ src/io/ed_opspec.F90
@@ -1342,10 +1342,10 @@ subroutine ed_opspec_misc
       write (unit=*,fmt='(a)') ' simulations only.  If that''s not what you wanted, change '
       write (unit=*,fmt='(a)') ' your IED_INIT_MODE variable on your ED2IN.                '
       write (unit=*,fmt='(a)') '==========================================================='
-   elseif ((ied_init_mode < -1 .or. ied_init_mode > 7) .and. &
+   elseif ((ied_init_mode < -1 .or. ied_init_mode > 6) .and. &
            (ied_init_mode /= 99 )) then
       write (reason,fmt='(a,1x,i4,a)')                                                     &
-                     'Invalid IED_INIT_MODE, it must be between -1 and 7. Yours is set to' &
+                     'Invalid IED_INIT_MODE, it must be between -1 and 6. Yours is set to' &
                     ,ied_init_mode,'...'
       call opspec_fatal(reason,'opspec_misc')
       ifaterr = ifaterr +1
